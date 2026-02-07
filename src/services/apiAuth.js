f7b7a6e9174e419a7250743448c63f6a964fc9e2
Thulani <thulanilugojolo@gmail.com>
2026-02-07 16:18:09 +0200

    fixed profiles tj

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_AUTH = axios.create({
   baseURL: import.meta.env.VITE_API_URL
     ? `${import.meta.env.VITE_API_URL}/api/auth`
-    : "http://localhost:5000/api/auth",
+    : "https://flowtoo-backend.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
